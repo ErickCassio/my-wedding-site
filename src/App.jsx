c19a3bf7ae608a57e42364a3416210a120613afc
Erick de Cássio <erickebb15@gmail.com>
2025-02-28 14:01:36 +0000

    Trying to turn Home page responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Home from "./components/Home.jsx";
+import Overview from "./components/Overview.jsx";
 
 function App() {
   return (
     <>
       <div>
         <Home />
+        <Overview />
       </div>
     </>
   );
