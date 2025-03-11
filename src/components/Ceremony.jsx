9b941ebbe5ce8e7891cd7f6edfa6735a248f7763
Erick de Cássio <erickebb15@gmail.com>
2025-03-11 14:03:39 +0000

    feat(ceremony): changes os ceremony page to make it responsive

--- src/components/Ceremony.jsx
+++ src/components/Ceremony.jsx
@@ -1,4 +1,4 @@
-import leaves from "../assets/images/leaves.jpg";
+// import leaves from "../assets/images/leaves.jpg";
 import map from "../assets/images/mapa.png";
 import calendar from "../assets/images/calendário.png";
 
@@ -11,21 +11,35 @@ function Ceremony() {
               font-bold
               text-[#1B1B6D]
               md:transform
-              md:-rotate-90"
+              md:-rotate-90
+              md:-ms-16
+              lg:-ms-0"
         >
           Nosso grande dia
         </h1>
       </div>
       <div className="md:w-8/12 flex flex-col justify-center items-center">
-        <img src={calendar} alt="" className="h-72 w-auto -ms-24" />
-        <div className="flex flex-col md:flex-row">
-          <div className="md:ps-4">
-            <h2>Cerimônia</h2>
-            <h6>Igreja Videira Viana do Castelo</h6>
+        <img
+          src={calendar}
+          alt=""
+          className="h-72 w-auto md:-ms-72 lg:-ms-80 xl:-ms-[28rem]"
+        />
+        <div className="flex flex-col md:flex-row space-y-10 md:space-y-0 md:space-x-8 xl:space-x-20 md:-ms-36 lg:-ms-80 xl:-ms-[28rem]">
+          <div className="md:ps-4 quicksand-font space-y-0.5">
+            <h2 className="montecarlo-font text-3xl pb-0.5 tracking-wider inline-block">
+              Cerimônia
+              <hr />
+            </h2>
+
+            <h6 className="font-bold">Igreja Videira Viana do Castelo</h6>
             <p>Praça 1º de Maio</p>
             <p>Viana do Castelo, Portugal</p>
           </div>
-          <img src={map} alt="" className="h-72 w-auto"/>
+          <img
+            src={map}
+            alt=""
+            className="h-48 w-72 lg:h-52 lg:w-80 border-2"
+          />
         </div>
       </div>
     </div>
